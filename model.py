8a6112a784a77db32b6cffb518b704cb115d3005
Avik Das <avikd957@gmail.com>
2026-09-04 13:57:40 +0530

    Step 144: lm_head_linear_forward

--- model.py
+++ model.py
@@ -1520,8 +1520,19 @@ def final_layernorm_forward(x, gamma, beta):
 
     return y, cache
 
-# Step 144 - lm_head_linear_forward (not yet solved)
-# TODO: implement
+# Step 144 - lm_head_linear_forward
+def lm_head_linear_forward(x, w_lm, b_lm):
+    """Project hidden states (B,T,d_model) to logits (B,T,vocab_size)."""
+    linear_result = linear_forward(x, w_lm)
+    bias_result = bias_add_forward(linear_result['y'], b_lm)
+
+    return {
+        'logits': bias_result['y'],
+        'cache': {
+            'x': x,
+            'w_lm': w_lm
+        }
+    }
 
 # Step 145 - full_model_forward (not yet solved)
 # TODO: implement
