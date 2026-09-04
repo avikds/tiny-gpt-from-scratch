366c05ee4cad95dd233b782d31ef17b62a3f43f4
Avik Das <avikd957@gmail.com>
2026-09-04 09:59:54 +0530

    Step 17: elementwise_add

--- model.py
+++ model.py
@@ -89,8 +89,10 @@ def slice_subblock(arr, r0, r1, c0, c1):
     """Return the sub-block arr[r0:r1, c0:c1] of a 2D array."""
     return arr[r0:r1, c0:c1]
 
-# Step 17 - elementwise_add (not yet solved)
-# TODO: implement
+# Step 17 - elementwise_add
+def elementwise_add(a, b):
+    """Return the elementwise sum of two same-shape arrays."""
+    return a + b
 
 # Step 18 - elementwise_multiply (not yet solved)
 # TODO: implement
