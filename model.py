a6c5d770ddb90ec69f2f406ea44418889f2a7910
Avik Das <avikd957@gmail.com>
2026-09-04 10:22:28 +0530

    Step 46: loop_fill_counts

--- model.py
+++ model.py
@@ -270,8 +270,14 @@ def allocate_count_matrix(vocab_size):
     """Allocate a (V, V) integer zero matrix for bigram counts."""
     return np.zeros((vocab_size, vocab_size), dtype=int)
 
-# Step 46 - loop_fill_counts (not yet solved)
-# TODO: implement
+# Step 46 - loop_fill_counts
+def loop_fill_counts(n_matrix, data):
+    """Increment n_matrix[curr, next] for every consecutive pair in data."""
+    for t in range(len(data) - 1):
+        curr = data[t]
+        next_token = data[t + 1]
+        n_matrix[curr, next_token] += 1
+    return n_matrix
 
 # Step 47 - vectorize_counts_add_at (not yet solved)
 # TODO: implement
