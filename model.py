ab4fb34b471d3aacf6e5af71f0208f908c67ed09
Avik Das <avikd957@gmail.com>
2026-09-04 13:21:15 +0530

    Step 91: layernorm_backward_implementation

--- model.py
+++ model.py
@@ -707,8 +707,10 @@ def layernorm_backward_full(dy, cache):
         'dbeta': dbeta
     }
 
-# Step 91 - layernorm_backward_implementation (not yet solved)
-# TODO: implement
+# Step 91 - layernorm_backward_implementation
+def layernorm_backward_implementation(d_out, cache):
+    # Reuse the complete LayerNorm backward implementation.
+    return layernorm_backward_full(d_out, cache)
 
 # Step 92 - create_token_embedding (not yet solved)
 # TODO: implement
