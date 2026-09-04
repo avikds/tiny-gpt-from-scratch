162ad2c8f806446ff6de7b63bd58af72b8a20fab
Avik Das <avikd957@gmail.com>
2026-09-04 13:15:06 +0530

    Step 80: bias_add_backward_db

--- model.py
+++ model.py
@@ -573,8 +573,11 @@ def bias_add_forward(x, b):
         }
     }
 
-# Step 80 - bias_add_backward_db (not yet solved)
-# TODO: implement
+# Step 80 - bias_add_backward_db
+def bias_add_backward_db(dy, cache):
+    """Compute db from upstream gradient dy for y = x + b."""
+    b_shape = cache['b_shape']
+    return np.sum(dy, axis=0).reshape(b_shape)
 
 # Step 81 - relu_forward (not yet solved)
 # TODO: implement
