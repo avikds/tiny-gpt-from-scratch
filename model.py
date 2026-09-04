4cdb5f6e49bdec03c20f77f145673567149e6862
Avik Das <avikd957@gmail.com>
2026-09-04 11:30:23 +0530

    Step 60: forward_logits_onehot

--- model.py
+++ model.py
@@ -362,8 +362,10 @@ def one_hot_encode_batch(ids, vocab_size):
     one_hot[np.arange(len(ids)), ids] = 1.0
     return one_hot
 
-# Step 60 - forward_logits_onehot (not yet solved)
-# TODO: implement
+# Step 60 - forward_logits_onehot
+def forward_logits_onehot(onehot, w_matrix):
+    # Compute logits for the neural bigram model as onehot @ W.
+    return matmul(onehot, w_matrix)
 
 # Step 61 - observe_lookup_equivalence (not yet solved)
 # TODO: implement
